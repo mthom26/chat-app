6d0c57076816be7d601e28207c3d3dd6d1b23e08
mthom26 <mthom2626@gmail.com>
2018-04-24 12:13:10 +0100

    Add conditional rendering to NavMenu

--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -1,21 +1,43 @@
 import React from 'react';
 import { StyleSheet, css } from 'aphrodite';
 import { Link } from 'react-router-dom';
+import { AuthUserContext } from '../contexts/index';
 
 const NavMenu = () => {
   return (
-    <div className={css(styles.navMenu)}>
-      <div>
-        LOGO
-      </div>
-      <div>
-        <div className={css(styles.navLinks)}>
-          <Link className={css(styles.navButton)} to="/home">HOME</Link>
-          <Link className={css(styles.navButton)} to="/signin">SIGN IN</Link>
-          <Link className={css(styles.navButton)} to="/signup">SIGN UP</Link>
-        </div>
-      </div>
-    </div>
+    <AuthUserContext>
+      {authUser => {
+        return (
+          <div className={css(styles.navMenu)}>
+            <div>
+              LOGO
+            </div>
+            <div>
+              <div className={css(styles.navLinks)}>
+                {authUser &&
+                  <Link
+                    className={css(styles.navButton)}
+                    to="/home">
+                    HOME
+                  </Link>}
+                {!authUser &&
+                  <Link
+                    className={css(styles.navButton)}
+                    to="/signin">
+                    SIGN IN
+                  </Link>}
+                {!authUser &&
+                  <Link
+                    className={css(styles.navButton)}
+                    to="/signup">
+                    SIGN UP
+                  </Link>}
+              </div>
+            </div>
+          </div>
+        );
+      }}
+    </AuthUserContext>
   );
 };
 
